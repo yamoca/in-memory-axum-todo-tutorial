a1b16310ad321527b5b9b4f020e20c3accf90dc0
leo <ljgulliver256@gmail.com>
2024-02-14 20:45:51 +0000

    update frontend with new todo structures

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,48 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const message = ref(null);
+import useTodos from './composables/useTodos';
 
-async function getMessage() {
-    const response = await fetch('http://localhost:3030');
-    
-    const json = await response.json();
-    message.value = json.message;
+const { addTodo, deleteTodo, todos, toggleCompleted } = useTodos();
+
+const newTodoTitle = ref('');
+
+function onInput(e: KeyboardEvent) {
+    if (e.key === 'Enter') {
+        addAndClear()
+    }
+}
+
+async function addAndClear() {
+    await addTodo(newTodoTitle.value);
+    newTodoTitle.value = '';
 }
 </script>
 
 <template>
-  <h1>Rust + Vue</h1>
-  <div v-if="message">
-      Received: {{ message }}
-  </div>
-  <div v-else>
-      No message
-  </div>
-  <button @click="getMessage()">Get message</button>
+    <h1>Rust by web</h1>
+    <h2>Todo</h2>
+    <ul class="todo-list">
+        <li v-for="todo in todos" :key="todo.id">
+            <label>
+                <input type="checkbox" :checked="todo.completed" @change="toggleCompleted(todo)" /> 
+                {{  todo.title }} {{ todo.completed ? '✅' : '' }}
+            </label>
+            <button @click="deleteTodo(todo)">Delete</button>
+        </li>
+    </ul>
+    <input type="text" v-model="newTodoTitle" @keypress="onInput" />
+    <button @click="addAndClear()">Add Todo</button>
 </template>
+
+<style>
+.todo-list {
+    list-style: none;
+    padding: 0;
+    text-align: left;
+}
+
+li {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
---
+++ frontend/src/composables/useTodos.ts
@@ -0,0 +1,65 @@
+import { computed, onMounted, ref } from "vue";
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const endpoint = (id?: number) => `http://localhost:3030/todos${typeof id == 'number' ? `/${id}` : ''}`;
+const request = (method: string, body?: any) => ({
+    method,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+});
+
+export default function useTodos() {
+    const todos = ref<Todo[]>([]);
+
+    async function getTodos() {
+        const response = await fetch(endpoint());
+        const json = await response.json();
+        todos.value = json;
+    }
+
+    async function toggleCompleted(todo: Todo) {
+        todo.completed = !todo.completed;
+        const response = await fetch(endpoint(todo.id), request('PATCH', todo));
+        const json = await response.json();
+        todos.value = json;
+    }
+
+    async function addTodo(newTodoTitle: string) {
+        if(newTodoTitle === '') {
+            return;
+        }
+
+        const response = await fetch(endpoint(), request('POST', {
+            title: newTodoTitle,
+        }))
+
+        const json = await response.json();
+        todos.value = json;
+    }
+
+    async function deleteTodo(todo: Todo) {
+         const response = await fetch(endpoint(todo.id), {
+            method: 'DELETEA'
+         });
+         const json = await response.json();
+         todos.value = json;
+    }
+
+    onMounted(() => {
+        getTodos();
+    })
+
+    return {
+        todos: computed(() => todos.value),
+        addTodo,
+        deleteTodo,
+        toggleCompleted,
+    }
+}
